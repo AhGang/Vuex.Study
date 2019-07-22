6c7b0abc30dae9a935eef8cf6d6417321a10250f
AhGang <474494594@qq.com>
2019-07-22 14:27:14 +0800

    git commit use vuex to implement Counter

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input v-model.number="inputNum">
+    <div>
+     <Counter></Counter>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter'
+
+export default {
+  components:{
+    Counter
+  },
+  name: 'app',
+  data(){
+    return {
+      inputNum : 0,
+
+    }
+  },
+  watch: {
+   
+  },
+  methods:{
+   
+  }
+}
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <button @click="calSum(1)">+</button>
+     <input id="num1" v-model="this.$store.state.count">
+    <button @click="calSum(-1)">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'Counter',
+ data(){
+    return {
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+  calSum(data){
+      this.$store.commit('increaseCount',data)
+     }
+}
+}
+</script>
+
+<style scoped>
+</style>
